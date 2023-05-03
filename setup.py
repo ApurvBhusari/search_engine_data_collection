574cede99b6aad5bc183b12f1d5970fdc73eb83d
apurva bhusari <apurvabhusari@gmail.com>
2023-05-03 14:52:41 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="src",
     version="0.0.1",
-    author="Ketan Gangal",
-    author_email="ketangangal98@gmail.com",
+    author="Apurv Bhusari",
+    author_email="apurvabhusari@gmail.com",
     packages=find_packages()
 )
